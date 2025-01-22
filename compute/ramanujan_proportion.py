2db37a0da0f142061cf9f23d66b9d48c1457b66d
Tadej <tadej.petric1@gmail.com>
2025-01-22 22:02:02 +0100

    computed 100k proportion for d=5

--- compute/ramanujan_proportion.py
+++ compute/ramanujan_proportion.py
@@ -3,10 +3,10 @@ from utils import second_eigenvalue
 
 
 attempts_per_size = 1000
-vertex_step = 20
-max_size = 1021
+vertex_step = 10_000
+max_size = 100_001
 d = 5
-starting_vertices =  966#d + 1 
+starting_vertices =  10_000#d + 1 
 assert starting_vertices > d
 
 processes = 10
